56bfa4f58b49847f43e419f0d031f32a4e177b5e
subin21cc <subin21cc@gmail.com>
2023-09-02 13:55:27 +0900

    chapter2-6.cpp: char[]을 string로 변경

--- 230819/chapter2-6/chapter2-6.cpp
+++ 230819/chapter2-6/chapter2-6.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-	char str1[100], str2[100];
+
+int main()
+{
+	string str1, str2;
 
 	cout << "새 암호를 입력하세요>>";
 	cin >> str1;
@@ -10,14 +12,12 @@ int main() {
 	cout << "새 암호를 다시 한 번 입력하세요>>";
 	cin >> str2;
 
-	for (int i = 0; str1[i] != NULL; i++)
-	{
-		if (str1[i] != str2[i])
-		{
-			cout << "같지 않습니다";
-			break;
-		}
-	}
+	if (str1 == str2)
+		cout << "같습니다\n";
+	else
+		cout << "같지 않습니다\n";
+
+	// cout << is_equal(str1, str2) ? "같습니다\n" : "같지 않습니다\n";
 
 	return 0;
 }
