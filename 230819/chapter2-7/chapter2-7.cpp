17ff1169e6137648e6734eec6f0a4207d46cb2f1
subin21cc <subin21cc@gmail.com>
2023-09-03 14:56:06 +0900

    chpater2-7.cpp: char[]을 string으로 변경

--- 230819/chapter2-7/chapter2-7.cpp
+++ 230819/chapter2-7/chapter2-7.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstring>
+#include <string>
 using namespace std;
 
 int main()
@@ -22,7 +23,7 @@ int main()
 	while (true)
 	{
 		cout << "종료하고싶으면 yes를 입력하세요>>";
-		cin.getline(answer);
+		getline(cin, answer);
 
 		if (answer == "yes")
 			break;
